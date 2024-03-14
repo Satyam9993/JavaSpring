2f78f20d8ef8f89f72f158c8ec806a37899306c3
unknown <satyamkatiyar9993.com>
2024-03-14 23:45:18 +0530

    getting beans concept

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,7 +1,31 @@
 package demo;
 
-public class Doctor {
+public class Doctor implements Staff{
+
+
+    String qualification;
+    Doctor(String qual){
+        qualification = qual;
+    }
+//    private Nurse nurse;
     public void assist(){
         System.out.println("Doctor is Assisting");
     }
+
+
+//    public String getQualification() {
+//        return qualification;
+//    }
+//
+//    public void setQualification(String qual) {
+//        this.qualification = qual;
+//    }
+
+//    public Nurse getNurse() {
+//        return nurse;
+//    }
+//
+//    public void setNurse(Nurse nurse) {
+//        this.nurse = nurse;
+//    }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -7,8 +7,21 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Doctor d = context.getBean(Doctor.class);
-        d.assist();
-        System.out.println("This is ");
+//        Doctor d = context.getBean(Doctor.class);
+//        d.assist();
+//        Nurse n = context.getBean(Nurse.class);
+//        Nurse n = (Nurse) context.getBean("nurse"); // Not used generally
+//        n.assist();
+
+//        Staff staff = context.getBean(Doctor.class);
+//        staff.assist();
+
+//        Doctor s = context.getBean(Doctor.class);
+//        s.assist();
+//        System.out.println(s.getQualification());
+//        s.getNurse().assist();
+
+        Doctor s = context.getBean(Doctor.class);
+        System.out.println(s.qualification);
     }
 }
