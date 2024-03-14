0e796158e34cb3ab022045583fb679737125c3da
satyamkatiyar <satyamkatiyar9993@gmail.com>
2024-03-15 00:59:28 +0530

    adding depencies to add post contruct method

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,12 +1,15 @@
 package demo;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 //@Scope(value = "singleton")
 @Scope(scopeName = "prototype")
-public class Doctor implements Staff{
+public class Doctor implements Staff, BeanNameAware {
 
 
     String qualification;
@@ -22,5 +25,16 @@ public class Doctor implements Staff{
 
     public void setQualification(String qual) {
         this.qualification = qual;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Set Bean Name Method is Called");
+    }
+
+
+    @PostConstruct
+    public void postConstructMethod(){
+        System.out.println("This is Post Construct");
     }
 }
